0afce3bf6f4c58cd1da0a1169ccfc2a02822aa82
Benedicte <benedicte.rivalain@gmail.com>
2018-10-08 12:01:33 +0200

    Implement App Component tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,52 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import configureStore from 'redux-mock-store';
+import { configure, shallow, mount } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
 import App from './App';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+/* fixture */
+import state from './testsFixtures/state';
+
+configure({ adapter: new Adapter() });
+
+function shallowComponent(store) {
+  return shallow(<App store={ store } />);
+}
+
+function mountComponent(store) {
+  return mount(<Provider store={ store }><App /></Provider>);
+}
+
+describe('Shallow Component App', () => {
+  let mockStore;
+  let store;
+  let component;
+
+  beforeEach(() => {
+    mockStore = configureStore();
+    store = mockStore(state);
+    component = shallowComponent(store);
+  });
+
+  test('renders without crashing', () => {
+    console.log(component)
+    expect(component).toBePresent();
+  });
+});
+
+describe('Mount App Container', () => {
+  let mockStore;
+  let store;
+  let component;
+
+  beforeEach(() => {
+    mockStore = configureStore();
+    store = mockStore(state);
+    component = mountComponent(store);
+  });
+
+  test('has been mounted', () => {
+    expect(component).toBePresent();
+  });
 });
---
+++ src/testsFixtures/state.js
@@ -0,0 +1,498 @@
+const state = {
+  movies: [
+    {
+      popularity: 494.735,
+      id: 335983,
+      video: false,
+      vote_count: 435,
+      vote_average: 6.7,
+      title: 'Venom',
+      release_date: '2018-10-05',
+      original_language: 'en',
+      original_title: 'Venom',
+      genre_ids: [
+        28,
+        27,
+        878,
+        53
+      ],
+      backdrop_path: '/VuukZLgaCrho2Ar8Scl9HtV3yD.jpg',
+      adult: false,
+      overview: 'When Eddie Brock acquires the powers of a symbiote, he will have to release his alter-ego “Venom” to save his life.',
+      poster_path: '/2uNW4WbgBXL25BAbXGLnLqX71Sw.jpg'
+    },
+    {
+      popularity: 326.68,
+      id: 363088,
+      video: false,
+      vote_count: 2475,
+      vote_average: 6.9,
+      title: 'Ant-Man and the Wasp',
+      release_date: '2018-07-06',
+      original_language: 'en',
+      original_title: 'Ant-Man and the Wasp',
+      genre_ids: [
+        28,
+        12,
+        35,
+        878,
+        10749,
+        10751
+      ],
+      backdrop_path: '/6P3c80EOm7BodndGBUAJHHsHKrp.jpg',
+      adult: false,
+      overview: 'Just when his time under house arrest is about to end, Scott Lang puts again his freedom at risk to help Hope van Dyne and Dr. Hank Pym dive into the quantum realm and try to accomplish, against time and any chance of success, a very dangerous rescue mission.',
+      poster_path: '/rv1AWImgx386ULjcf62VYaW8zSt.jpg'
+    },
+    {
+      popularity: 193.745,
+      id: 346910,
+      video: false,
+      vote_count: 452,
+      vote_average: 5.3,
+      title: 'The Predator',
+      release_date: '2018-09-14',
+      original_language: 'en',
+      original_title: 'The Predator',
+      genre_ids: [
+        28,
+        35,
+        27,
+        878
+      ],
+      backdrop_path: '/f4E0ocYeToEuXvczZv6QArrMDJ.jpg',
+      adult: false,
+      overview: 'From the outer reaches of space to the small-town streets of suburbia, the hunt comes home. Now, the universe’s most lethal hunters are stronger, smarter and deadlier than ever before, having genetically upgraded themselves with DNA from other species. When a young boy accidentally triggers their return to Earth, only a ragtag crew of ex-soldiers and a disgruntled science teacher can prevent the end of the human race.',
+      poster_path: '/wMq9kQXTeQCHUZOG4fAe5cAxyUA.jpg'
+    },
+    {
+      popularity: 172.706,
+      id: 299536,
+      video: false,
+      vote_count: 8544,
+      vote_average: 8.3,
+      title: 'Avengers: Infinity War',
+      release_date: '2018-04-27',
+      original_language: 'en',
+      original_title: 'Avengers: Infinity War',
+      genre_ids: [
+        28,
+        12,
+        14,
+        878
+      ],
+      backdrop_path: '/lmZFxXgJE3vgrciwuDib0N8CfQo.jpg',
+      adult: false,
+      overview: 'As the Avengers and their allies have continued to protect the world from threats too large for any one hero to handle, a new danger has emerged from the cosmic shadows: Thanos. A despot of intergalactic infamy, his goal is to collect all six Infinity Stones, artifacts of unimaginable power, and use them to inflict his twisted will on all of reality. Everything the Avengers have fought for has led up to this moment - the fate of Earth and existence itself has never been more uncertain.',
+      poster_path: '/7WsyChQLEftFiDOVTGkv3hFpyyt.jpg'
+    },
+    {
+      popularity: 142.404,
+      id: 353081,
+      video: false,
+      vote_count: 1786,
+      vote_average: 7.3,
+      title: 'Mission: Impossible - Fallout',
+      release_date: '2018-07-27',
+      original_language: 'en',
+      original_title: 'Mission: Impossible - Fallout',
+      genre_ids: [
+        28,
+        12,
+        53
+      ],
+      backdrop_path: '/a9fRqsl6P27qxbDSb4pBXNv9Fwl.jpg',
+      adult: false,
+      overview: 'When an IMF mission ends badly, the world is faced with dire consequences. As Ethan Hunt takes it upon himself to fulfill his original briefing, the CIA begin to question his loyalty and his motives. The IMF team find themselves in a race against time, hunted by assassins while trying to prevent a global catastrophe.',
+      poster_path: '/AkJQpZp9WoNdj7pLYSj1L0RcMMN.jpg'
+    },
+    {
+      popularity: 139.936,
+      id: 351286,
+      video: false,
+      vote_count: 3771,
+      vote_average: 6.5,
+      title: 'Jurassic World: Fallen Kingdom',
+      release_date: '2018-06-22',
+      original_language: 'en',
+      original_title: 'Jurassic World: Fallen Kingdom',
+      genre_ids: [
+        28,
+        12,
+        27,
+        878
+      ],
+      backdrop_path: '/3s9O5af2xWKWR5JzP2iJZpZeQQg.jpg',
+      adult: false,
+      overview: 'Three years after the demise of Jurassic World, a volcanic eruption threatens the remaining dinosaurs on the isla Nublar, so Claire Dearing, the former park manager, recruits Owen Grady to help prevent the extinction of the dinosaurs once again.',
+      poster_path: '/c9XxwwhPHdaImA2f1WEfEsbhaFB.jpg'
+    },
+    {
+      popularity: 132.911,
+      id: 348350,
+      video: false,
+      vote_count: 2054,
+      vote_average: 6.7,
+      title: 'Solo: A Star Wars Story',
+      release_date: '2018-05-25',
+      original_language: 'en',
+      original_title: 'Solo: A Star Wars Story',
+      genre_ids: [
+        28,
+        12,
+        80,
+        878,
+        37,
+        10749
+      ],
+      backdrop_path: '/96B1qMN9RxrAFu6uikwFhQ6N6J9.jpg',
+      adult: false,
+      overview: 'Through a series of daring escapades deep within a dark and dangerous criminal underworld, Han Solo meets his mighty future copilot Chewbacca and encounters the notorious gambler Lando Calrissian.',
+      poster_path: '/3IGbjc5ZC5yxim5W0sFING2kdcz.jpg'
+    },
+    {
+      popularity: 119.735,
+      vote_count: 30,
+      video: false,
+      poster_path: '/1PrlKvPUbDQAqFQBCyyt68hLLl.jpg',
+      id: 401478,
+      adult: false,
+      backdrop_path: '/W8V8d9bjLZURgEGbIB5TC70yaj.jpg',
+      original_language: 'en',
+      original_title: 'Death Race 4: Beyond Anarchy',
+      genre_ids: [
+        28
+      ],
+      title: 'Death Race 4: Beyond Anarchy',
+      vote_average: 5.4,
+      overview: 'Black Ops specialist Connor Gibson infiltrates a maximum security prison to take down legendary driver Frankenstein in a violent and brutal car race.',
+      release_date: '2018-01-30'
+    },
+    {
+      popularity: 113.011,
+      id: 284054,
+      video: false,
+      vote_count: 8425,
+      vote_average: 7.4,
+      title: 'Black Panther',
+      release_date: '2018-02-16',
+      original_language: 'en',
+      original_title: 'Black Panther',
+      genre_ids: [
+        28,
+        12,
+        14,
+        878
+      ],
+      backdrop_path: '/b6ZJZHUdMEFECvGiDpJjlfUWela.jpg',
+      adult: false,
+      overview: 'King T\'Challa returns home from America to the reclusive, technologically advanced African nation of Wakanda to serve as his country\'s new leader. However, T\'Challa soon finds that he is challenged for the throne by factions within his own country as well as without. Using powers reserved to Wakandan kings, T\'Challa assumes the Black Panther mantel to join with girlfriend Nakia, the queen-mother, his princess-kid sister, members of the Dora Milaje (the Wakandan \'special forces\') and an American secret agent, to prevent Wakanda from being dragged into a world war.',
+      poster_path: '/uxzzxijgPIY7slzFvMotPv8wjKA.jpg'
+    },
+    {
+      popularity: 110.666,
+      id: 447200,
+      video: false,
+      vote_count: 911,
+      vote_average: 6.1,
+      title: 'Skyscraper',
+      release_date: '2018-07-13',
+      original_language: 'en',
+      original_title: 'Skyscraper',
+      genre_ids: [
+        28,
+        18,
+        53
+      ],
+      backdrop_path: '/oMKFQmoVgB69fyXfSMu0lGlHJP2.jpg',
+      adult: false,
+      overview: 'Framed and on the run, a former FBI agent must save his family from a blazing fire in the world\'s tallest building.',
+      poster_path: '/5LYSsOPzuP13201qSzMjNxi8FxN.jpg'
+    },
+    {
+      popularity: 101.353,
+      id: 383498,
+      video: false,
+      vote_count: 5399,
+      vote_average: 7.5,
+      title: 'Deadpool 2',
+      release_date: '2018-05-18',
+      original_language: 'en',
+      original_title: 'Deadpool 2',
+      genre_ids: [
+        28,
+        35,
+        878
+      ],
+      backdrop_path: '/3P52oz9HPQWxcwHOwxtyrVV1LKi.jpg',
+      adult: false,
+      overview: 'Wisecracking mercenary Deadpool battles the evil and powerful Cable and other bad guys to save a boy\'s life.',
+      poster_path: '/to0spRl1CMDvyUbOnbb4fTk3VAd.jpg'
+    },
+    {
+      popularity: 96.519,
+      id: 442249,
+      video: false,
+      vote_count: 949,
+      vote_average: 5.8,
+      title: 'The First Purge',
+      release_date: '2018-07-04',
+      original_language: 'en',
+      original_title: 'The First Purge',
+      genre_ids: [
+        28,
+        27,
+        878,
+        53
+      ],
+      backdrop_path: '/r4clWIxKeWoqrHspQP0XunuUmB7.jpg',
+      adult: false,
+      overview: 'To push the crime rate below one percent for the rest of the year, the New Founding Fathers of America test a sociological theory that vents aggression for one night in one isolated community. But when the violence of oppressors meets the rage of the others, the contagion will explode from the trial-city borders and spread across the nation.',
+      poster_path: '/litjsBoiydO6JlO70uOX4N3WnNL.jpg'
+    },
+    {
+      popularity: 92.824,
+      id: 345940,
+      video: false,
+      vote_count: 1117,
+      vote_average: 6,
+      title: 'The Meg',
+      release_date: '2018-08-10',
+      original_language: 'en',
+      original_title: 'The Meg',
+      genre_ids: [
+        28,
+        878,
+        53
+      ],
+      backdrop_path: '/rH79sB6Nkx4cMW3JzsUy7wK0rhX.jpg',
+      adult: false,
+      overview: 'A deep sea submersible pilot revisits his past fears in the Mariana Trench, and accidentally unleashes the seventy foot ancestor of the Great White Shark believed to be extinct.',
+      poster_path: '/eyWICPcxOuTcDDDbTMOZawoOn8d.jpg'
+    },
+    {
+      popularity: 79.433,
+      id: 463272,
+      video: false,
+      vote_count: 62,
+      vote_average: 6.4,
+      title: 'Johnny English Strikes Again',
+      release_date: '2018-10-26',
+      original_language: 'en',
+      original_title: 'Johnny English Strikes Again',
+      genre_ids: [
+        28,
+        12,
+        35,
+        10751
+      ],
+      backdrop_path: '/yCOLqh5MOGyYdo58Ap0aWvKop9h.jpg',
+      adult: false,
+      overview: 'Disaster strikes when a criminal mastermind reveals the identities of all active undercover agents in Britain. The secret service can now rely on only one man—Johnny English. Currently teaching at a minor prep school, Johnny springs back into action to find the mysterious hacker. For this mission to succeed, he’ll need all of his skills—what few he has—as the man with yesterday’s analogue methods faces off against tomorrow’s digital technology.',
+      poster_path: '/tCBxnZwLiY1BOKw3tH6AxHZdqPh.jpg'
+    },
+    {
+      popularity: 74.67,
+      id: 260513,
+      video: false,
+      vote_count: 2754,
+      vote_average: 7.6,
+      title: 'Incredibles 2',
+      release_date: '2018-06-15',
+      original_language: 'en',
+      original_title: 'Incredibles 2',
+      genre_ids: [
+        28,
+        12,
+        16,
+        10751
+      ],
+      backdrop_path: '/mabuNsGJgRuCTuGqjFkWe1xdu19.jpg',
+      adult: false,
+      overview: 'Elastigirl springs into action to save the day, while Mr. Incredible faces his greatest challenge yet – taking care of the problems of his three children.',
+      poster_path: '/x1txcDXkcM65gl7w20PwYSxAYah.jpg'
+    },
+    {
+      popularity: 74.463,
+      id: 400535,
+      video: false,
+      vote_count: 494,
+      vote_average: 6.6,
+      title: 'Sicario: Day of the Soldado',
+      release_date: '2018-06-29',
+      original_language: 'en',
+      original_title: 'Sicario: Day of the Soldado',
+      genre_ids: [
+        28,
+        80,
+        18,
+        53
+      ],
+      backdrop_path: '/tnwMCH4yLBY4qpe6Nr4n66u4U3f.jpg',
+      adult: false,
+      overview: 'Agent Matt Graver teams up with operative Alejandro Gillick to prevent Mexican drug cartels from smuggling terrorists across the United States border.',
+      poster_path: '/msqWSQkU403cQKjQHnWLnugv7EY.jpg'
+    },
+    {
+      popularity: 64.216,
+      id: 402900,
+      video: false,
+      vote_count: 1909,
+      vote_average: 6.9,
+      title: 'Ocean\'s Eight',
+      release_date: '2018-06-08',
+      original_language: 'en',
+      original_title: 'Ocean\'s Eight',
+      genre_ids: [
+        28,
+        35,
+        80,
+        53
+      ],
+      backdrop_path: '/scQf03Fm3jeyv4FH04qvi4fp4wh.jpg',
+      adult: false,
+      overview: 'Debbie Ocean, a criminal mastermind, gathers a crew of female thieves to pull off the heist of the century at New York\'s annual Met Gala.',
+      poster_path: '/MvYpKlpFukTivnlBhizGbkAe3v.jpg'
+    },
+    {
+      popularity: 61.484,
+      id: 460885,
+      video: false,
+      vote_count: 140,
+      vote_average: 6.6,
+      title: 'Mandy',
+      release_date: '2018-09-13',
+      original_language: 'en',
+      original_title: 'Mandy',
+      genre_ids: [
+        28,
+        27,
+        53
+      ],
+      backdrop_path: '/mqbAmIiToU2R4VwZGgCqsfy6Yh4.jpg',
+      adult: false,
+      overview: 'The Shadow Mountains, 1983. Red and Mandy lead a loving and peaceful existence; but when their pine-scented haven is savagely destroyed, Red is catapulted into a phantasmagoric journey filled with bloody vengeance and laced with fire.',
+      poster_path: '/m0yf7J7HsKeK6E81SMRcX8vx6mH.jpg'
+    },
+    {
+      popularity: 61.456,
+      id: 406761,
+      video: false,
+      vote_count: 115,
+      vote_average: 6,
+      title: 'Hotel Artemis',
+      release_date: '2018-06-08',
+      original_language: 'en',
+      original_title: 'Hotel Artemis',
+      genre_ids: [
+        28,
+        878,
+        53
+      ],
+      backdrop_path: '/9E4mLJY4ZiQDPi2KR8cWr7mHF8o.jpg',
+      adult: false,
+      overview: 'Set in riot-torn, near-future Los Angeles, \'Hotel Artemis\' follows the Nurse, who runs a secret, members-only emergency room for criminals.',
+      poster_path: '/vaZMpZOhhkKOYz0MHCx4vDABwgP.jpg'
+    },
+    {
+      popularity: 59.176,
+      id: 427641,
+      video: false,
+      vote_count: 2055,
+      vote_average: 6.2,
+      title: 'Rampage',
+      release_date: '2018-04-13',
+      original_language: 'en',
+      original_title: 'Rampage',
+      genre_ids: [
+        28,
+        12,
+        14,
+        878
+      ],
+      backdrop_path: '/wrqUiMXttHE4UBFMhLHlN601MZh.jpg',
+      adult: false,
+      overview: 'Primatologist Davis Okoye shares an unshakable bond with George, the extraordinarily intelligent, silverback gorilla who has been in his care since birth.  But a rogue genetic experiment gone awry mutates this gentle ape into a raging creature of enormous size.  To make matters worse, it’s soon discovered there are other similarly altered animals.  As these newly created alpha predators tear across North America, destroying everything in their path, Okoye teams with a discredited genetic engineer to secure an antidote, fighting his way through an ever-changing battlefield, not only to halt a global catastrophe but to save the fearsome creature that was once his friend.',
+      poster_path: '/3gIO6mCd4Q4PF1tuwcyI3sjFrtI.jpg'
+    }
+  ],
+  genres: [
+    {
+      id: 28,
+      name: 'Action'
+    },
+    {
+      id: 12,
+      name: 'Adventure'
+    },
+    {
+      id: 16,
+      name: 'Animation'
+    },
+    {
+      id: 35,
+      name: 'Comedy'
+    },
+    {
+      id: 80,
+      name: 'Crime'
+    },
+    {
+      id: 99,
+      name: 'Documentary'
+    },
+    {
+      id: 18,
+      name: 'Drama'
+    },
+    {
+      id: 10751,
+      name: 'Family'
+    },
+    {
+      id: 14,
+      name: 'Fantasy'
+    },
+    {
+      id: 36,
+      name: 'History'
+    },
+    {
+      id: 27,
+      name: 'Horror'
+    },
+    {
+      id: 10402,
+      name: 'Music'
+    },
+    {
+      id: 9648,
+      name: 'Mystery'
+    },
+    {
+      id: 10749,
+      name: 'Romance'
+    },
+    {
+      id: 878,
+      name: 'Science Fiction'
+    },
+    {
+      id: 10770,
+      name: 'TV Movie'
+    },
+    {
+      id: 53,
+      name: 'Thriller'
+    },
+    {
+      id: 10752,
+      name: 'War'
+    },
+    {
+      id: 37,
+      name: 'Western'
+    }
+  ],
+  genreId: '28'
+}
